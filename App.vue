1fc8a342b6aea854e00ee90f8c89c2539d85d033
Anmol Jain <anmol@geekyants.com>
2020-12-30 11:08:30 +0530

    app.vue updated

--- App.vue
+++ App.vue
@@ -4,10 +4,7 @@
     <!-- <box-animation /> -->
     <slot-animation />
     <!-- <scroll-animation /> -->
-    <!-- <hexagon /> -->
     <!-- <progress-loader /> -->
-    <!-- <vue-native-anim /> -->
-    <!-- <example /> -->
   </safe-area-view>
 </template>
 
@@ -16,27 +13,15 @@ import BoxAnimation from './BoxAnimation'
 import { ImageBackground } from 'react-native'
 import ScrollAnimation from './ScrollAnimation'
 import SlotAnimation from './SlotAnimation'
-import Game from './game.jpg'
 import ProgressLoader from './ProgressLoader.vue'
-import Hexagon from './Hexagon'
-import VueNativeAnim from './VueNativeAnim'
-import Example from './Example'
 
 export default {
-  data () {
-    return {
-        Game
-        }
-  },
   components:{
-    Hexagon,
     BoxAnimation,
     ScrollAnimation,
     SlotAnimation,
     ImageBackground,
     ProgressLoader,
-    VueNativeAnim,
-    Example
   }
 }
 </script>
